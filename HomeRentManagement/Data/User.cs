966b63c341b26c63952765efa48f4d823b3dcc10
iqbal-emon <ihemon281@gmail.com>
2024-05-17 14:02:40 -0700

    done of add validation

--- HomeRentManagement/Data/User.cs
+++ HomeRentManagement/Data/User.cs
@@ -7,10 +7,22 @@ namespace HomeRentManagement.Data
     {
         [Key]
         public int UserId { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
+        [Required(ErrorMessage = "User Name is required")]
         public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "Phone is required")]
+        [Phone(ErrorMessage = "Invalid phone number")]
+        [StringLength(11, ErrorMessage = "Phone number cannot be longer than 11 characters")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
         public string Address { get; set; }
 
         public int RoleID { get; set; } = 2;
